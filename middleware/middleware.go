a382caa52c32d788cceb49d5426d2f1147921b77
Bastian Situmorang <bastianpaskal@gmail.com>
2020-10-18 16:56:16 +0700

    feat: add error handler for http (#4)

---
+++ middleware/middleware.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/situmorangbastian/ambarita/models"
+)
+
+// ErrMiddleware returns custom middleware for Fiber that generate HTTP error response
+// with HTTP status code.
+func ErrMiddleware(ctx *fiber.Ctx, err error) error {
+	errResponse := models.DefaultErrorResponse()
+	errResponse.Message = err.Error()
+
+	// Check error based on error type
+	switch err.(type) {
+	case models.ErrorValidation:
+		errResponse.Status = http.StatusBadRequest
+		errResponse.Data = map[string]interface{}{}
+		return ctx.Status(errResponse.Status).JSON(errResponse)
+	}
+
+	switch err {
+	case models.ErrBadRequest:
+		errResponse.Status = http.StatusBadRequest
+	case models.ErrNotFound:
+		errResponse.Status = http.StatusNotFound
+	default:
+		log.Error(err)
+		errResponse.Message = "Internal Server Error"
+	}
+
+	return ctx.Status(errResponse.Status).JSON(errResponse)
+}
