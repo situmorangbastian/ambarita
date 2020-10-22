de2f19253bdc47810216ee178c406c52cf1703b1
Bastian Paskal Situmorang <bastianpaskal@gmail.com>
2020-10-22 17:23:56 +0700

    refactor: implement errors wrap

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/golang-migrate/migrate v3.5.4+incompatible
 	github.com/google/uuid v1.1.1
 	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pkg/errors v0.8.1
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/viper v1.7.0
 	github.com/stretchr/testify v1.6.1 // indirect
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/situmorangbastian/ambarita/models"
@@ -15,22 +16,22 @@ func ErrMiddleware(ctx *fiber.Ctx, err error) error {
 	errResponse := models.DefaultErrorResponse()
 	errResponse.Message = err.Error()
 
-	// Retreive the custom statuscode if it's an fiber.*Error
-	if e, ok := err.(*fiber.Error); ok {
+	// Retreive the custom response if it's an fiber.*Error
+	if e, ok := errors.Cause(err).(*fiber.Error); ok {
 		errResponse.Message = e.Error()
 		errResponse.Status = e.Code
 		return ctx.Status(errResponse.Status).JSON(errResponse)
 	}
 
 	// Check error based on error type
-	switch err.(type) {
+	switch errors.Cause(err).(type) {
 	case models.ErrorValidation:
 		errResponse.Status = http.StatusBadRequest
 		errResponse.Data = map[string]interface{}{}
 		return ctx.Status(errResponse.Status).JSON(errResponse)
 	}
 
-	switch err {
+	switch errors.Cause(err) {
 	case models.ErrBadRequest:
 		errResponse.Status = http.StatusBadRequest
 	case models.ErrNotFound:
