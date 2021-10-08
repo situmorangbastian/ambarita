2991b7b086213bbe91d331fac12ce26edae95d82
Bastian Situmorang <bastianpaskal@gmail.com>
2021-10-08 17:40:08 +0700

    feat: integrate vercel with mongo (#22)

--- api/ambarita/main.go
+++ api/ambarita/main.go
@@ -1,14 +1,39 @@
 package ambarita
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/situmorangbastian/ambarita/api/vercelhttp"
+	articleRepository "github.com/situmorangbastian/ambarita/article/repository"
+	articleUsecase "github.com/situmorangbastian/ambarita/article/usecase"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	mongoURI := os.Getenv("MONGO_URI")
+
+	mongoClient, err := mongo.Connect(
+		context.Background(),
+		options.Client().ApplyURI(mongoURI).
+			SetConnectTimeout(2*time.Second).
+			SetServerSelectionTimeout(3*time.Second),
+	)
+	if err != nil {
+		log.Fatal("Mongo connection failed: ", err.Error())
+	}
+
+	mongoDBName := os.Getenv("MONGO_DB_NAME")
+
+	ar := articleRepository.NewMongoRepository(mongoClient.Database(mongoDBName))
+	vercelhttp.ArticleUsecase = articleUsecase.NewArticleUsecase(ar)
+
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/posts", vercelhttp.FetchAllArticles).Methods(http.MethodGet)
